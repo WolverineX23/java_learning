e74b92be902d92346230d76ca983c2b2c0906c03
WolverineX23 <547036318@qq.com>
2024-05-12 12:01:18 +0800

    add：二叉树的最大路径和

--- ms-algorithm/src/main/java/org/example/algorithm/TreeAlgo/MaxLen.java
+++ ms-algorithm/src/main/java/org/example/algorithm/TreeAlgo/MaxLen.java
@@ -1,12 +1,14 @@
 package org.example.algorithm.TreeAlgo;
 
 /**
- * 二叉树的最大深度 - 力扣104
- * 二叉树的直接 - 力扣543 - 直径：任意两个节点之间最长路径的 长度
+ * 二叉树的最大深度 - 力扣 104
+ * 二叉树的直接 - 力扣 543 - 直径：任意两个节点之间最长路径的 长度
+ * 二叉树的最大路径和 - 力扣 124
  *
  */
 public class MaxLen {
     int res = 0;
+    int ans = Integer.MIN_VALUE;
 
     // 深度
     public int dfs(TreeNode root) {
@@ -36,5 +38,24 @@ public class MaxLen {
 
         dfs2(root);
         return res;
+    }
+
+    // 最大路径和
+    public int dfs3(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int L = dfs3(root.left);
+        int R = dfs3(root.right);
+
+        ans = Math.max(ans, L + R + root.val);  // 更新最大路径和
+
+        return Math.max(0, Math.max(L, R) + root.val);  // 返回以当前节点为一端的最大路径和，若为负值则减断，设为 0
+    }
+
+    public int maxPathSum(TreeNode root) {
+
+        dfs3(root);
+        return ans;
     }
 }
