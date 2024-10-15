7fe4ab761bec20882db578d2458192b059e6a628
WolverineX23 <547036318@qq.com>
2024-10-15 11:11:43 +0800

    add：线段树区间查询

--- ms-algorithm/src/main/java/org/example/algorithm/SegmentTreeAlgo/Main.java
+++ ms-algorithm/src/main/java/org/example/algorithm/SegmentTreeAlgo/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Integer[] nums = {-2, 0, 3, -5, 2, -1};
         SegmentTree<Integer> segmentTree = new SegmentTree<>(nums, (a, b) -> a + b);
+
+        // 测试线段树构建结果
         System.out.println(segmentTree);
+
+        // 测试区间查询
+        System.out.println(segmentTree.query(0, 2));        // 1
+        System.out.println(segmentTree.query(4, 5));        // 1
+        System.out.println(segmentTree.query(1, 4));        // 0
     }
 }
--- ms-algorithm/src/main/java/org/example/algorithm/SegmentTreeAlgo/SegmentTree.java
+++ ms-algorithm/src/main/java/org/example/algorithm/SegmentTreeAlgo/SegmentTree.java
@@ -42,12 +42,51 @@ public class SegmentTree<E> {
 //        int mid = (l + r) / 2;
         int mid = l + (r - l) / 2;
 
-        // 递归调用
+        // 递归调用：这里的设计使得通常 左子树节点数 >= 右子树节点数
         buildSegmentTree(leftTreeIndex, l , mid);
         buildSegmentTree(rightTreeIndex, mid + 1, r);
 
         // 泛型设计，tree[treeIndex]的具体赋值取决于用户的具体需求，因此引入一个融合器接口
         tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
+    }
+
+    /**
+     * 区间查询：返回待查询区间 [queryL, queryR] 得值
+     *
+     * @param queryL
+     * @param queryR
+     * @return
+     */
+    public E query(int queryL, int queryR) {
+        // 边界检查
+        if (queryL < 0 || queryL >= data.length || queryR < 0 || queryR >= data.length || queryL > queryR) {
+            throw new IllegalArgumentException("Index is illegal");
+        }
+
+        // 递归函数, 从根节点开始
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+
+    // 在以 treeIndex 为根得线段树中 [l...r] 的范围里，搜索区间 [queryL...queryR] 的值
+    private E query(int treeIndex, int l, int r, int queryL, int queryR) {
+        if(l == queryL && r == queryR) {
+            return tree[treeIndex];
+        }
+
+        int mid = l + (r - l) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        // 判断带查询区间的位置
+        if (queryL > mid) {             // 带查询区间落在右孩子那边
+            return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+        } else if (queryR <= mid) {     // 带查询区间落在左孩子那边
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+        } else {
+            E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+            E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+            return merger.merge(leftResult, rightResult);
+        }
     }
 
     private int leftChild(int index) {
