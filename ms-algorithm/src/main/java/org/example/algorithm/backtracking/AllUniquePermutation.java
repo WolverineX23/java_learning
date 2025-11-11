b9e1fd417edeedc0eed1bd5e8488742182ecfff5
wx <547036318@qq.com>
2025-11-11 23:33:09 +0800

    add 全排列

---
+++ ms-algorithm/src/main/java/org/example/algorithm/backtracking/AllUniquePermutation.java
@@ -0,0 +1,47 @@
+package org.example.algorithm.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 46. 全排列
+ */
+public class AllUniquePermutation {
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+
+        if (len == 0) {
+            return res;
+        }
+
+        boolean[] used = new boolean[len];
+        Arrays.fill(used, false);
+
+        List<Integer> path = new ArrayList<>();
+        dfs(nums, len, 0, path, res, used);
+
+        return res;
+    }
+
+    private void dfs(int[] nums, int len, int depth, List<Integer> path, List<List<Integer>> res, boolean[] used) {
+        if (len == depth) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (!used[i]) {
+                path.add(nums[i]);
+                used[i] = true;
+
+                dfs(nums, len, depth + 1, path, res, used);
+                used[i] = false;
+                path.remove(depth);
+            }
+        }
+    }
+}
